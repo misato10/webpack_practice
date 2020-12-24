3fc08ff926951897e04fb1a3eebcab3f21559939
misato.uematsu <misato.uematsu@gpol.co.jp>
2020-12-24 15:42:19 +0900

    ES6, Raect, Vue, etc...

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,18 @@
 // モジュールの読み込み
+import Vue from 'vue';
+import VueApp from'./VueApp.vue';
+
 import my from './my.js';
+import './reactApp.jsx';
 import '../stylesheets/main.scss';
 
 console.log('webpack');
 
 // モジュールを実行
 my();
+
+
+new Vue({
+  el: '#vue-root',
+  render: (h) => h(VueApp),
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,11 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // clean-webpack-plugin から　CleanWebpackPluginのクラスのみ使用する
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/javascripts/main.js',
   //ファイルの出力場所 
   // ※出力場所は絶対パスで指定する必要がある
@@ -16,6 +19,32 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'vue-loader',
+          }
+        ],
+      },
+      {
+        test: /\.js/,
+        // /node_modules/配下は除外する
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                // 0.25%以上シェアを持っていて、サポートの終了していないブラウザを対象とする
+                ['@babel/preset-env', { "targets": "> 0.25%, not dead" }],
+                '@babel/preset-react'
+              ],
+            }
+          }
+        ]
+      },
+      {
         // testでファイル名の検知を行う、.css、sass、scssファイルを検知
         test: /\.(css|sass|scss)/,
         use: [
@@ -25,7 +54,11 @@ module.exports = {
           },
           // .cssファイルにはcss-loaderを使用し読み込む
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
+            options: {
+              // ソースマップを出力する（trueで）
+              sourceMap: false
+            }
           },
           {
             loader: 'sass-loader'
@@ -33,13 +66,22 @@ module.exports = {
         ] 
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
               name: 'images/[name].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              }
             }
           }
         ]
@@ -77,6 +119,7 @@ module.exports = {
       template: './src/templates/members/taro.pug',
       filename: 'members/taro.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin()
   ]
 }
